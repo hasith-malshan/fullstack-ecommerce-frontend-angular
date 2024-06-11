9729bc87d811037960ce762ee7f793bb53c1ed71
hmr53 <hasher22542@gmail.com>
2024-06-11 09:23:04 +0530

    Months and Years for credit card subscribed from ShopFormService.

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { last } from 'rxjs';
+import { ShopFormService } from 'src/app/services/shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -10,10 +11,15 @@ import { last } from 'rxjs';
 export class CheckoutComponent implements OnInit {
   checkoutFromGroup: FormGroup;
 
-  totalPrice : number = 0;
-  totalQuantity : number = 0;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private shopFormService: ShopFormService
+  ) {
     this.checkoutFromGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: [''],
@@ -45,20 +51,32 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const startMonth: number = new Date().getMonth() + 1;
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe((data) => {
+      console.log(JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+
+    this.shopFormService.getCreditCardYears().subscribe((data) => {
+      console.log(JSON.stringify(data));
+      this.creditCardYears = data;
+    });
+  }
 
   onSubmit() {
     console.log('Handling form Submition');
     console.log(this.checkoutFromGroup.get('customer')?.value.email);
   }
 
-  copyShoppingAddressToBillingAddress(event : any) {
+  copyShoppingAddressToBillingAddress(event: any) {
     if (event.target.checked) {
       this.checkoutFromGroup.controls['billingAddress'].setValue(
         this.checkoutFromGroup.controls['shippingAddress'].value
       );
-    }else{
-       this.checkoutFromGroup.controls['billingAddress'].reset();
+    } else {
+      this.checkoutFromGroup.controls['billingAddress'].reset();
     }
   }
 }
